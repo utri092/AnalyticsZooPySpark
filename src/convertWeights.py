e6ee63ed08ed68b42ecf96838b12655c82c1bd39
fsan110 <fsan110@aucklanduni.ac.nz>
2020-12-29 19:52:47 +1300

    Refactored convertWeights.py

--- src/convertWeights.py
+++ src/convertWeights.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from zoo.pipeline.nnframes import NNModel
 
+############### STEP 1: Create Keras Model with Same Architecure as BigDL Model ############
 df = pd.read_csv("../resources/datasets/dataset-1_converted.csv")
 
 trainDf, testDf = train_test_split(df, test_size=0.2)
@@ -35,10 +35,12 @@ inferenceWeights = model.get_weights()
 print("Model1 Weights before setting from Cluster! \n")
 print(inferenceWeights)
 
-from zoo.pipeline.api.keras.models import Model
-from zoo.pipeline.api.keras.layers import Input, Dense
+
+############### STEP 2: Load Trained BigDL Model ###############
+
 from bigdl.util.common import init_engine, create_spark_conf
 from pyspark.sql import SparkSession
+from bigdl.nn.layer import *
 
 conf = create_spark_conf() \
     .setAppName("Spark_Basic_Learning") \
@@ -52,43 +54,22 @@ spark = SparkSession.builder \
     .config(conf=conf) \
     .getOrCreate()
 
-# Init Big DL Engine
 init_engine()
 
-# parkingInput2 = Input(shape=(inputs,))
-# print(parkingInput2.shape)
-#
-# denseLayer2 = Dense(output_dim=inputs, activation="relu")
-# hidden2 = denseLayer2(parkingInput2)
-#
-# lastLayer2 = Dense(output_dim=outputs,activation="relu")(hidden2)
-# model2 = Model(input=parkingInput2, output=lastLayer2, name="functionalModel2")
-#
-#
-# model2.compile(optimizer='adam', loss='mean_squared_error')
-#
-# # Set Tensorboard
-# log_dir = "../resources/board/model_log"
-# app_name = "zooKeras"
-# model2.set_tensorboard(log_dir = log_dir, app_name=app_name)
-#
-# model2.fit(x=x.to_numpy(), y=y.to_numpy(), nb_epoch=2, distributed=False)
-# model2.summary()
+bigDlModel = Model.loadModel(modelPath="../resources/savedModel/bigdl/trainedNN.bigdl", weightPath="../resources/savedModel/bigdl/trainedNN.bin")
 
-from tensorflow.keras.models import load_model
-model2 = load_model("../resources/savedModels/bigdl/model.h5")
+print("Loaded BigDL Model !")
 
-# nnModel = NNModel(model2)
+zooWeights = bigDlModel.get_weights()
 
-# zooWeights = nnModel.model.get_weights()
-# zooWeights = model2.get_weights()
-#
-# for i in range(len(zooWeights)):
-#
-#     zooWeights[i] = zooWeights[i].reshape(inferenceWeights[i].shape)
-#
-# model.set_weights(zooWeights)
-#
-# model.save(filepath="../resources/savedModels/keras/inferenceModel.h5")
+############### STEP 3:  Reshape Weights & Bias Arrays of BigDL Model to Keras Model's Original Shape
 
-# print("Saved Model!")
+for i in range(len(zooWeights)):
+
+    zooWeights[i] = zooWeights[i].reshape(inferenceWeights[i].shape)
+
+model.set_weights(zooWeights)
+
+model.save(filepath="../resources/savedModels/keras/inferenceModel.h5")
+
+print("Saved Model!")
